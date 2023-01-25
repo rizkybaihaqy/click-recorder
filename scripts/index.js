af6bb01b0b5d39c28cdb3a85cf624929c4a3ca6e
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-25 17:47:59 +0700

    expose dist

--- scripts/index.js
+++ scripts/index.js
@@ -23,5 +23,5 @@ const ClickRecorder = (name) =>
       y: ev.layerY,
       name,
     };
-    await fetchJSON("post")("http://localhost:3000/api/save-click")(data);
+    await fetchJSON("post")("https://muzukd.deta.dev/api/save-click")(data);
   });
