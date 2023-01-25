49b47fe538c19ccf3135040c487b0a72e8df2979
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-25 15:53:34 +0700

    add morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import apiRouter from "./routes/api";
 import webRouter from "./routes/web";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use("/", webRouter);
 app.use("/api", cors({ origin: "*" }), express.json(), apiRouter);
