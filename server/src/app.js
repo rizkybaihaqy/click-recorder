79c8b2a4cce543f61240a649ab33d8432a38dbb5
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-01-25 17:08:02 +0700

    add dashboard with login and logout

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,36 @@
-import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import * as Eta from "eta";
+import express from "express";
 import morgan from "morgan";
+import path from "path";
 import apiRouter from "./routes/api";
 import webRouter from "./routes/web";
 
 const app = express();
 
+app.set("view engine", "eta");
+app.engine("eta", Eta.renderFile);
+app.set("views", path.join(__dirname, "views"));
+
 app.use(morgan("dev"));
 
-app.use("/", webRouter);
-app.use("/api", cors({ origin: "*" }), express.json(), apiRouter);
+app.use(
+  "/",
+  cookieParser(),
+  express.urlencoded({ extended: true }),
+  (req, res, next) => {
+    const { name = '' } = req.cookies;
+    res.locals = { name, ...res.locals };
+    next();
+  },
+  webRouter
+);
+app.use(
+  "/api",
+  cors({ origin: "*" }),
+  express.json(),
+  apiRouter
+);
 
 export default app;
--- server/src/routes/web.js
+++ server/src/routes/web.js
@@ -1,6 +1,24 @@
 import { Router } from "express";
+import db from "../db";
 
 const router = Router();
+
+router.get("/dashboard", async (_, res) => {
+  const { name } = res.locals;
+  const data = await db.fetch({ name });
+  res.render("dashboard", { name, data });
+});
+
+router.post("/login", (req, res) => {
+  const { name } = req.body;
+  res.cookie("name", name);
+  res.redirect("/dashboard");
+});
+
+router.post("/logout", (_, res) => {
+  res.clearCookie("name");
+  res.redirect("/dashboard");
+});
 
 router.get("/ping", (_, res) => {
   res.send("pong");
